c3550ecb98e59e2e997eca367de6a03ac84d1652
Ilia Reingold <ilia.reingold@gmail.com>
2014-04-16 16:58:39 -0400

    nothing broken yet! added user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140416201735) do
+ActiveRecord::Schema.define(version: 20140416205048) do
 
   create_table "products", force: true do |t|
     t.string   "name"
     t.string   "description"
     t.string   "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
