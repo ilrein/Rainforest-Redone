21db19b1f2c1da1e4e829b23c066377a177ff086
Ilia Reingold <ilia.reingold@gmail.com>
2014-04-16 19:06:09 -0400

    worked out sessions/logins

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+ 
   root "products#index"
   
+  resources :sessions, :only => [:new, :create, :destroy]
   resources :products
-  resources :users, :only => [:new, :create]
+  resources :users, :only => [:new, :create, :destroy]
 end
