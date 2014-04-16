3b705ca13afed0cc71e3d198adf056804a4d889a
Ilia Reingold <ilia.reingold@gmail.com>
2014-04-16 19:33:13 -0400

    mostly done the tutz

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
  
+  get 'reviews/show'
+
+  get 'reviews/new'
+
+  get 'reviews/edit'
+
   root "products#index"
   
   resources :sessions, :only => [:new, :create, :destroy]
-  resources :products
+  resources :products do
+  	resources :reviews, :except => [:index]
+  end
   resources :users, :only => [:new, :create, :destroy]
 end
